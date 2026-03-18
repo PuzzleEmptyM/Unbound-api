855bd53c9e031d3f066a8ec6b9d53fe6b2cd8c71
PuzzleEmptyM <emptympuzzle@gmail.com>
2026-03-18 13:39:29 -0500

    Rename DATABASE_URL to STORAGE_DATABASE_URL

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
 import { neon } from '@neondatabase/serverless'
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL environment variable is not set')
+if (!process.env.STORAGE_DATABASE_URL) {
+  throw new Error('STORAGE_DATABASE_URL environment variable is not set')
 }
 
-export const sql = neon(process.env.DATABASE_URL)
+export const sql = neon(process.env.STORAGE_DATABASE_URL)
