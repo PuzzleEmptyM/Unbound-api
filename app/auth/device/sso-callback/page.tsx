f5e81bea71e241042aad08305c71e8016d5562ca
PuzzleEmptyM <emptympuzzle@gmail.com>
2026-03-18 14:41:05 -0500

    Remove invalid props from AuthenticateWithRedirectCallback

--- app/auth/device/sso-callback/page.tsx
+++ app/auth/device/sso-callback/page.tsx
@@ -3,10 +3,5 @@
 import { AuthenticateWithRedirectCallback } from '@clerk/nextjs'
 
 export default function SSOCallback() {
-  return (
-    <AuthenticateWithRedirectCallback
-      afterSignInUrl="/auth/device/callback"
-      afterSignUpUrl="/auth/device/callback"
-    />
-  )
+  return <AuthenticateWithRedirectCallback />
 }
